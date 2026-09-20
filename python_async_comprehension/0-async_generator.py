f0167220ff562d56a287c66a8c15df8f3031fc34
asimhamidwo8 <asimhamidwo8@gmail.com>
2026-09-20 08:04:16 +0300

    python_async_comprehension: fix annotations and pycodestyle

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -2,11 +2,13 @@
 """Async generator"""
 import asyncio
 import random
-from typing import AsyncGenerator
+from typing import Generator
 
 
-async def async_generator() -> AsyncGenerator[float, None]:
-    """Loop 10 times, wait 1 second each time, then yield a random number between 0 and 10"""
+async def async_generator() -> Generator[float, None, None]:
+    """Loop 10 times, wait 1 second each time,
+    then yield a random number between 0 and 10.
+    """
     for _ in range(10):
         await asyncio.sleep(1)
         yield random.uniform(0, 10)
